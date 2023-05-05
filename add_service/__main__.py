ea3fa287ede7db5914e4da98f91f9b57ba232162
Lei <ylxx@live.com>
2023-05-05 13:49:16 +0800

    u

--- add_service/__main__.py
+++ add_service/__main__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __description__ = "Python tool for simply adding startup item by systemd."
 __license__ = "MIT"
 __author__ = "DIYer22"
